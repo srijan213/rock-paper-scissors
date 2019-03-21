36a41a333e5041893f18d9aea5f6eab1b9ca0a97
srijan213 <srijanmanandhar12@gmail.com>
2019-03-21 23:36:26 +0545

    Added game logic - js and Changed initial output message - HTML

--- javascript/app.js
+++ javascript/app.js
@@ -12,22 +12,48 @@ let userScore = 0;
 let computerScore = 0;
 let userInput = "";
 let computerInput = "";
+let choices = ["rock", "paper", "scissors"];
 
 function computerInputGenerator() {
   computerInput = Math.floor(Math.random() * 3);
+  if (computerInput === 0) computerInput = choices[0];
+  else if (computerInput === 1) computerInput = choices[1];
+  else computerInput = choices[2];
   return computerInput;
 }
 
+function resultCalculator(userInput, computerInput) {
+  if (userInput === computerInput) output(`ITS A DRAW`);
+  else if (userInput == choices[0] && computerInput == choices[1])
+    output(`YOU LOST! PAPER BEATS ROCK`);
+  else if (userInput == choices[0] && computerInput == choices[2])
+    output(`YOU WON! ROCK BEATS SCISSORS`);
+  else if (userInput == choices[1] && computerInput == choices[2])
+    output(`YOU LOST! SCISSORS BEATS PAPER`);
+  else if (userInput == choices[1] && computerInput == choices[0])
+    output("YOU WON! PAPER BEATS ROCK");
+  else if (userInput == choices[2] && computerInput == choices[0])
+    output("YOU LOST! ROCK BEATS SCISSORS");
+  else if (userInput == choices[2] && computerInput == choices[1])
+    output("YOU WON! SCISSORS BEATS PAPER");
+}
+
+function output(outputMessage) {
+  outputMessage_div.innerHTML = outputMessage;
+}
 // EVENTLISTENERS
 
 rock_img.addEventListener("click", function() {
-  console.log("rock");
+  userInput = choices[0];
+  resultCalculator(userInput, computerInputGenerator());
 });
 
 paper_img.addEventListener("click", function() {
-  console.log("paper");
+  userInput = choices[1];
+  resultCalculator(userInput, computerInputGenerator());
 });
 
 scissors_img.addEventListener("click", function() {
-  console.log("scissors");
+  userInput = choices[2];
+  resultCalculator(userInput, computerInputGenerator());
 });
