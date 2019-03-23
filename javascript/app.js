49557ab7b7dc9e7965974a3b769c1ee50d8e3baa
Srijan Manandhar <srijanmanandhar12@gmail.com>
2019-03-23 12:43:40 +0545

    Completed v1.0 of RPS Game

--- javascript/app.js
+++ javascript/app.js
@@ -6,13 +6,15 @@ const rock_img = document.getElementById("rock--icon");
 const paper_img = document.getElementById("paper--icon");
 const scissors_img = document.getElementById("scissors--icon");
 const outputMessage_div = document.getElementById("output--message");
+const choices_div = document.getElementById("choices");
+const reset_button = document.getElementById("reset--button");
 
 // VARIABLES
 let userScore = 0;
 let computerScore = 0;
 let userInput = "";
 let computerInput = "";
-let choices = ["rock", "paper", "scissors"];
+let choices = ["Rock", "Paper", "Scissors"];
 
 function computerInputGenerator() {
   computerInput = Math.floor(Math.random() * 3);
@@ -22,20 +24,45 @@ function computerInputGenerator() {
   return computerInput;
 }
 
+//choices[0] = ROCK, choices[1] = PAPER,  choices[2] = SCISSORS
+
 function resultCalculator(userInput, computerInput) {
+  let userWon = false;
   if (userInput === computerInput) output(`ITS A DRAW`);
-  else if (userInput == choices[0] && computerInput == choices[1])
-    output(`YOU LOST! PAPER BEATS ROCK`);
-  else if (userInput == choices[0] && computerInput == choices[2])
-    output(`YOU WON! ROCK BEATS SCISSORS`);
-  else if (userInput == choices[1] && computerInput == choices[2])
-    output(`YOU LOST! SCISSORS BEATS PAPER`);
-  else if (userInput == choices[1] && computerInput == choices[0])
-    output("YOU WON! PAPER BEATS ROCK");
-  else if (userInput == choices[2] && computerInput == choices[0])
-    output("YOU LOST! ROCK BEATS SCISSORS");
-  else if (userInput == choices[2] && computerInput == choices[1])
-    output("YOU WON! SCISSORS BEATS PAPER");
+  else {
+    if (userInput == choices[0] && computerInput == choices[1])
+      output(`YOU LOST! PAPER BEATS ROCK`);
+    else if (userInput == choices[1] && computerInput == choices[2])
+      output(`YOU LOST! SCISSORS BEATS PAPER`);
+    else if (userInput == choices[2] && computerInput == choices[0])
+      output("YOU LOST! ROCK BEATS SCISSORS");
+    else if (userInput == choices[0] && computerInput == choices[2]) {
+      output(`YOU WON! ROCK BEATS SCISSORS`);
+      userWon = true;
+    } else if (userInput == choices[1] && computerInput == choices[0]) {
+      output("YOU WON! PAPER BEATS ROCK");
+      userWon = true;
+    } else if (userInput == choices[2] && computerInput == choices[1]) {
+      output("YOU WON! SCISSORS BEATS PAPER");
+      userWon = true;
+    }
+    scoreHandler(userWon);
+    outputScore();
+  }
+  outputChoices(`USER: ${userInput} -|- COMPUTER: ${computerInput}`);
+}
+
+function scoreHandler(userWon) {
+  userWon ? (userScore += 1) : (computerScore += 1);
+}
+
+function outputScore() {
+  userScore_span.innerHTML = userScore;
+  compScore_span.innerHTML = computerScore;
+}
+
+function outputChoices(message) {
+  choices_div.innerHTML = message;
 }
 
 function output(outputMessage) {
@@ -56,4 +83,12 @@ paper_img.addEventListener("click", function() {
 scissors_img.addEventListener("click", function() {
   userInput = choices[2];
   resultCalculator(userInput, computerInputGenerator());
+});
+
+reset_button.addEventListener("click", function() {
+  if (confirm("Are you sure you want to reset")) {
+    userScore = 0;
+    computerScore = 0;
+    outputScore();
+  }
 });
